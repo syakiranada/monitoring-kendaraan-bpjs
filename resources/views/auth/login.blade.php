af7f4b0c23fc07566478745aba76868c8df9ce70
syakiranada <sharkeeraaa@gmail.com>
2025-02-04 21:03:55 +0700

    modifikasi Controllers/Auth (dashboard jadi beranda), modifikasi halaman login dan register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,7 +14,7 @@
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Kata Sandi')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -28,20 +28,28 @@
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                <span class="ms-2 text-sm text-gray-600">{{ __('Ingat saya') }}</span>
             </label>
         </div>
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
+                    {{ __('Lupa kata sandi?') }}
                 </a>
             @endif
 
             <x-primary-button class="ms-3">
-                {{ __('Log in') }}
+                {{ __('Masuk') }}
             </x-primary-button>
+        </div>
+
+        <!-- Link ke Registrasi -->
+        <div class="mt-4 text-center">
+            <p class="text-sm text-gray-600">
+                Belum punya akun? 
+                <a href="{{ route('register') }}" class="text-indigo-600 hover:text-indigo-900 underline">Daftar di sini</a>.
+            </p>
         </div>
     </form>
 </x-guest-layout>
